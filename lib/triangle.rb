28f34dddc738339ad157ed653f024e416d2eb6cd
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-04 15:39:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,7 @@ class Triangle
     elsif @side1 == 0 && @side2 == 0 && @side3 == 0
       begin
         raise TriangleError
-      rescue TriangleEror => error 
+      rescue TriangleError => error 
         puts error.message
       end
     end
